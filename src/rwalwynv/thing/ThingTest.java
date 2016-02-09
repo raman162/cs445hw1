a5390e9f85a656664b2e451cfd4ebe537e37128b
Raman Walwyn-Venugopal <ramanvenu94@gmail.com>
2016-02-08 23:33:53 -0600

    Finished test coverage

--- src/rwalwynv/thing/ThingTest.java
+++ src/rwalwynv/thing/ThingTest.java
@@ -1,5 +1,18 @@
 package rwalwynv.thing;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class ThingTest {
+
+	Thing thing= new Thing("rasta");
+	
+	
+	@Test
+	public void testThing() {
+	assertEquals(thing.getName(), "rasta");
+	assertEquals(thing.toString(), "rasta Thing");
+	}
 
 }
